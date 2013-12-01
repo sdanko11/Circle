a88b6af69bf4ef3992a7c436d7f2729754465687
steve <stevedanko1@gmail.com>
2013-11-30 21:30:54 -0500

    created a class to calculate the circumfrence of a circle

---
+++ circle.rb
@@ -0,0 +1,23 @@
+require 'pry'
+
+class Circle
+
+  def initialize(radius)
+    @radius = radius.to_i
+  end
+
+  def diameter
+    @radius * 2
+  end
+
+  def circumfrence
+    circumfrence = 2*3.14*@radius
+  end
+
+  def area
+    area = 3.14*@radius*@radius
+  end
+
+end
+
+binding.pry
